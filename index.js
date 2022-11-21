6af0f54b2db1497277271f68ebe0536af82d91e2
Saimplice Ruud Christopher <saimpliceudemy@gmail.com>
2022-11-21 13:18:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require("express");
-const app = express();
-const product = require("./api/product");
+const GUN     = require('gun')
+const express = require('express')
+const gun     = GUN()
+const app     = express()
 
-app.use(express.json({ extended: false }));
+app.use(express.json())
 
-app.use("/api/product", product);
+app.get('/',(req,res)=>{
+    res.send('Hello user !')
+})
+
+app.post('/subscribe',(req,res)=>{
+    gun.get(req.body.sender).set({cohort:req.body.id})
+    res.send(200)
+})
+
+app.post('/cohort',(req,res)=>{
+
+    const allowance = []
+    gun.get(req.body.sender).map().on((data, key) => {
+        if(data && !allowance.includes(data.cohort)){
+            allowance.push(data.cohort)
+        }
+    });
+    res.json(allowance)
+    
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
